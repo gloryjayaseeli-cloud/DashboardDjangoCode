1c82b58c5c441c0837a16feef87b8a6ffc09ae95
Glory <glory.jayaseeli@tigeranalytics.com>
2025-09-18 18:10:18 +0530

    url added

--- myprojectdashboard/settings.py
+++ myprojectdashboard/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -68,7 +69,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware', 
     
 ]
 
@@ -149,6 +149,7 @@ REST_FRAMEWORK = {
 
 
 CORS_ALLOWED_ORIGINS = [
+    "https://yourdashboardmonitor.netlify.app/",
     "http://localhost:3000",
         "http://localhost:3001",
     "http://127.0.0.1:3000", 
