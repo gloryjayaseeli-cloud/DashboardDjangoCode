15ca413737b478147b63876d1933159155a38911
Glory <glory.jayaseeli@tigeranalytics.com>
2025-10-28 13:58:15 +0530

    path change

--- myprojectdashboard/urls.py
+++ myprojectdashboard/urls.py
@@ -18,8 +18,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
 urlpatterns = [
+    path('', lambda request: HttpResponse("OK", status=200)),
     path('admin/', admin.site.urls),
    
     path('api/', include('api.urls')),
